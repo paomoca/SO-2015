a9b62cbf51819698a29265a43f6a81bda96c2d5e
diegomartin <diegomartinrecillas@outlook.com>
2015-11-29 14:37:01 -0600

    Se agrego la excepcion del inode

---
+++ InodeFileTooBigException.java
@@ -0,0 +1,21 @@
+
+
+public class InodeFileTooBigException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String message;
+	
+	public InodeFileTooBigException(String message) {
+		this.message = message;
+	}
+	
+	@Override
+	public String toString() {
+		return this.message;
+	}
+
+}
