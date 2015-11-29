07d29973c316018ed087a05d2b40395c61e9a566
paomoca <paomoca.16@gmail.com>
2015-11-29 13:51:54 -0600

    Este es el estado inicial de la compu de pao.

---
+++ src/DeviceInitializationException.java
@@ -0,0 +1,18 @@
+public class DeviceInitializationException extends Exception {
+	
+	private String mensaje;
+	
+	private static final long serialVersionUID = 1L;
+	
+	public DeviceInitializationException(String mensaje){
+		
+		this.mensaje = mensaje;
+		
+	}
+	
+	@Override
+	public String toString() {
+		return mensaje;
+	}
+
+}
