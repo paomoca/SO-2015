b253e11639fa24a47936566d5b077c2f329353ca
diegomartin <diegomartinrecillas@outlook.com>
2015-12-01 15:12:51 -0600

    Primera etapa del visualizador del disco

--- so/gui/fsframe/FSFrame.java
+++ so/gui/fsframe/FSFrame.java
@@ -1,46 +1,31 @@
-//package so.gui.fsframe;
-//
-//
-//import so.gui.drawpanel.*;
-//import so.gui.drawpanel.Rectangle;
-//
-//import javax.swing.*;
-//import java.awt.*;
-//
-///**
-// * Created by diegomartin on 12/1/15.
-// */
-//public class FSFrame extends JFrame {
-//
-//    private DrawPanel dp;
-//
-//    public FSFrame() {
-//
-//        dp = DrawPanel.getInstance();
-//        dp.setCanvasSize(500, 500);
-//
-//        JPanel dpHolder = new JPanel();;
-//        dpHolder.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-//        dpHolder.setLayout(new BorderLayout(0, 0));
-//        dpHolder.add(dp, BorderLayout.CENTER);
-//        dpHolder.setPreferredSize(new Dimension(dp.getWidth() + 2, dp
-//                .getHeight() + 2));
-//
-//        JPanel content = new JPanel();
-//        content.setLayout(new BorderLayout());
-//        content.add(dpHolder, BorderLayout.CENTER);
-//
-//
-//        this.add(content, BorderLayout.CENTER);
-//        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        this.setTitle("File System GUI");
-//        this.pack();
-//
-//        this.drawTest();
-//
-//    }
-//
-//    public void drawTest() {
-//        Rectangle.execute(0,0,100,100);
-//    }
-//}
+package so.gui.fsframe;
+
+import javax.swing.*;
+import java.awt.*;
+import so.gui.grid.Grid;
+
+/**
+ * Created by diegomartin on 12/1/15.
+ */
+public class FSFrame extends JFrame {
+
+    private Grid grid;
+
+    public FSFrame() {
+
+        grid = new Grid();
+
+        JPanel content = new JPanel();
+        content.setLayout(new BorderLayout());
+        content.add(grid, BorderLayout.CENTER);
+
+
+        this.add(content, BorderLayout.CENTER);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("File System GUI");
+        this.pack();
+
+
+
+    }
+}
--- so/gui/grid/Grid.java
+++ so/gui/grid/Grid.java
@@ -2,6 +2,8 @@ package so.gui.grid;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.*;
 
 /**
@@ -10,41 +12,91 @@ import java.util.*;
 public class Grid extends JPanel {
 
 
-    private int width = -1;
-    private int height = -1;
-    private int pWidth = -1;
-    private int pHeight = -1;
+    private int gridWidth = 500;
+    private int gridHeight = 500;
+
+    private int cellWidth = 10;
+    private int cellHeight = 10;
+
+    private int gridOffset = 10;
+    private int columns = gridWidth/cellWidth;
     private java.util.List<Point> fillCells;
 
     public Grid() {
         fillCells = new ArrayList<>();
+
+        this.setPreferredSize(new Dimension(gridWidth + (gridOffset * 2), gridHeight + (gridOffset * 2)));
+
+        this.addMouseListener(new BlockClickListener());
+
+        System.out.print("Total blocks = " + (gridWidth/cellWidth)*(gridHeight/cellHeight) + "\n");
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         for (Point fillCell : fillCells) {
-            int cellX = 10 + (fillCell.x * 10);
-            int cellY = 10 + (fillCell.y * 10);
+            int cellX = gridOffset + (fillCell.x * cellWidth);
+            int cellY = gridOffset + (fillCell.y * cellHeight);
             g.setColor(Color.RED);
-            g.fillRect(cellX, cellY, 10, 10);
+            g.fillRect(cellX, cellY, cellWidth, cellHeight);
         }
 
         g.setColor(Color.BLACK);
-        g.drawRect(10, 10, 800, 500);
+        g.drawRect(gridOffset, gridOffset, gridWidth, gridHeight);
 
-        for (int i = 10; i <= 800; i += 10) {
-            g.drawLine(i, 10, i, 510);
+        for (int i = gridOffset; i <= gridWidth + gridOffset; i += cellWidth) {
+            g.drawLine(i, gridOffset, i, gridHeight + gridOffset);
         }
 
-        for (int i = 10; i <= 500; i += 10) {
-            g.drawLine(10, i, 810, i);
+        for (int i = gridOffset; i <= gridHeight + gridOffset; i += cellHeight) {
+            g.drawLine(gridOffset, i, gridWidth + gridOffset, i);
         }
+
     }
 
     public void fillCell(int x, int y) {
         fillCells.add(new Point(x, y));
         repaint();
+    }
+
+    class BlockClickListener implements MouseListener {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            int row = (e.getY()/cellWidth) - 1;
+            int column = (e.getX()/cellHeight) - 1;
+
+
+            int section = (columns * row) + column ;
+
+            if (section >= 0 && section < 2500) {
+                System.out.println(section);
+            }
+
+            fillCell(column,row);
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
     }
 
 
