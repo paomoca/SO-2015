400b7e1438dddf70a234e245911ce0cf82de0a7b
paomoca <paomoca.16@gmail.com>
2015-12-03 04:31:15 -0600

    en teoria ya borra

--- so/filesystem/filemanagment/FileController.java
+++ so/filesystem/filemanagment/FileController.java
@@ -159,6 +159,7 @@ public class FileController {
 			if((blockAddress = DiskFreeSpaceManager.getInstance().firstFreeBlock()) != -1){
 				System.out.println("BLOCK ADDRESS ASIGNED: "+blockAddress);
 				
+				
 				DiskController.getInstance().rawWriteBlockPayload(blockAddress, dataBuffer, numberOfBytesRead);
 				
 				//Sets control bytes.
